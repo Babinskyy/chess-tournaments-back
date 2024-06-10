1048279b82482849075664442a07cef26336293f
Babinskyy <janbbnsk@protonmail.com>
2024-06-10 21:53:11 +0200

    fix status updating in tournament

--- src/utils/updatePlayerStatus.ts
+++ src/utils/updatePlayerStatus.ts
@@ -1,13 +1,17 @@
 import { Player, PlayerStatus } from "../types/types.types";
 import { findTournamentByUsername } from "./findTournamentByUsername";
-import { activePlayers } from "../socket/onConnection";
+import { activePlayers, activeTournaments } from "../socket/onConnection";
 
 export const updatePlayerStatus = (player: Player, status: PlayerStatus) => {
   const tournament = findTournamentByUsername(player.username);
 
-  tournament?.players.forEach((playerInTournament) => {
-    if (playerInTournament.username === player.username) {
-      playerInTournament.status = status;
+  activeTournaments.forEach((searchedTournament) => {
+    if (searchedTournament.id === tournament?.id) {
+      searchedTournament?.players.forEach((playerInTournament) => {
+        if (playerInTournament.username === player.username) {
+          playerInTournament.status = status;
+        }
+      });
     }
   });
 
