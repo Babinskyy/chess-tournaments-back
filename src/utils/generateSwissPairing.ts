397b659f9676527b554db4a765e558ced8c9399b
Babinskyy <janbbnsk@protonmail.com>
2024-07-05 11:38:04 +0200

    delete unnecessary package import

--- src/utils/generateSwissPairing.ts
+++ src/utils/generateSwissPairing.ts
@@ -1,6 +1,5 @@
 import { v4 } from "uuid";
 import { Player } from "../types/types.types";
-import { Swiss } from "tournament-pairings";
 
 export const generateSwissPairings = () => {
   const playersArr = [
